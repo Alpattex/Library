768b192bba13515c72e9b96c6d02952c365e730f
odinstudent <34312554+Alpattex@users.noreply.github>
2023-10-02 00:47:17 +0300

    Creating Appealing Window for "NEW BOOK"

--- javascript.js
+++ javascript.js
@@ -1,34 +1,58 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const newBookBtn = document.getElementById('newBookBtn');
-    const bookList = document.getElementById('bookList');
+function createBook(title, author, pages, read) {
+    const book = document.createElement('div');
+    book.classList.add('book');
 
-    newBookBtn.addEventListener('click', function () {
-        const title = prompt('Enter book title:');
-        const author = prompt('Enter author:');
-        const pages = prompt('Enter number of pages:');
-        const read = confirm('Have you read this book?');
+    const authorSection = createSection('Author', author);
+    const titleSection = createSection('Title', title);
+    const pagesSection = createSection('Pages', pages);
+    const statusSection = createSection('Status', read ? 'Read' : 'Not Read');
+
+    [authorSection, titleSection, pagesSection, statusSection].forEach(section => book.appendChild(section));
+
+    return book;
+}
+
+function createSection(label, content) {
+    const section = document.createElement('p');
+    section.textContent = `${label}: ${content}`;
+    return section;
+}
+
+newBookBtn.addEventListener('click', function () {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const form = document.createElement('form');
+    form.innerHTML = `
+        <label for="title">Title:</label>
+        <input type="text" id="title" required>
+
+        <label for="author">Author:</label>
+        <input type="text" id="author" required>
+
+        <label for="pages">Pages:</label>
+        <input type="number" id="pages" required>
+
+        <label for="read">Have you read it?</label>
+        <input type="checkbox" id="read">
+
+        <button type="submit">Add Book</button>
+    `;
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const pages = document.getElementById('pages').value;
+        const read = document.getElementById('read').checked;
 
         if (title && author && pages) {
-            const book = createBook(title, author, pages, read);
-            bookList.appendChild(book);
+            const newBook = createBook(title, author, pages, read);
+            bookList.appendChild(newBook);
+            modal.remove();
         }
     });
 
-    function createBook(title, author, pages, read) {
-        const book = document.createElement('div');
-        book.classList.add('book');
-
-        const bookInfo = document.createElement('p');
-        bookInfo.textContent = `${title} by ${author}\n${pages} pages`;
-
-        const readStatus = document.createElement('p');
-        readStatus.textContent = read ? 'Read' : 'Not Read';
-        readStatus.style.color = read ? '#27AE60' : '#E74C3C';
-
-        book.appendChild(bookInfo);
-        book.appendChild(readStatus);
-
-        return book;
-    }
+    modal.appendChild(form);
+    document.body.appendChild(modal);
 });
-
